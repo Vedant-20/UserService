e96e3a94352e79f6f5d9fa08d66a927baeb9fdb6
Vedant <vedantwarjurkar@gmail.com>
2025-09-06 12:33:06 +0530

    called hotel service to get info

--- src/main/java/com/hotel/user/service/services/impl/UserServiceImpl.java
+++ src/main/java/com/hotel/user/service/services/impl/UserServiceImpl.java
@@ -1,11 +1,20 @@
 package com.hotel.user.service.services.impl;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import com.hotel.user.service.entities.Hotel;
+import com.hotel.user.service.entities.Rating;
 import com.hotel.user.service.entities.User;
 import com.hotel.user.service.exceptions.ResourceNotFoundException;
 import com.hotel.user.service.repositories.UserRepository;
@@ -16,6 +25,11 @@ public class UserServiceImpl implements UserService {
 	
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	private Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
 
 	@Override
 	public User saveUser(User user) {
@@ -33,7 +47,24 @@ public class UserServiceImpl implements UserService {
 	@Override
 	public User getUser(String userId) {
 		
-		return userRepository.findById(userId).orElseThrow(()->new ResourceNotFoundException("User not found with given id: " + userId));
+		User user = userRepository.findById(userId).orElseThrow(()->new ResourceNotFoundException("User not found with given id: " + userId));
+		Rating[] ratingsOfUser=restTemplate.getForObject("http://RATINGSERVICE/ratings/users/"+user.getUserId(), Rating[].class);
+		logger.info(" {}",ratingsOfUser);
+		List<Rating> ratings =Arrays.stream(ratingsOfUser).toList();
+		List<Rating> ratingList=ratings.stream().map(rating ->{
+			//api call to hotel service to get the hotel
+			//set the hotel to rating
+			//return the rating
+			//http://localhost:8082/hotels/:hotelId
+			ResponseEntity<Hotel> hotelDetails=restTemplate.getForEntity("http://HOTELSERVICE/hotels/"+rating.getHotelId(),Hotel.class);
+			Hotel hotel=hotelDetails.getBody();		
+			logger.info("response status code: {}",hotelDetails.getStatusCode());
+			rating.setHotel(hotel);
+			return rating;
+			
+		}).collect(Collectors.toList());
+		user.setRatings(ratingList);
+		return user;
 	}
 
 	@Override
